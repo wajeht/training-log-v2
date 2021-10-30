9a5baf23712c8749a045ed4e2a00ffc0aeae2dfd
wajeht <zombyard@gmail.com>
2021-10-30 17:52:49 -0500

    chore: add error handling on db connect

--- bin/server.js
+++ bin/server.js
@@ -12,10 +12,17 @@ const success = chalk.black.bgGreen;
 (async () => {
   try {
     const conn = await db.raw("select 1");
+
+    if (!conn) {
+      throw new Error("Db cannot connect!");
+    }
+
     app.listen(PORT, () => {
       log(success(`Db connected successfully!`));
       log(success(`Server started on http://localhost:${PORT}`));
     });
+
+
   } catch (err) {
     log(error(err));
   }
