0133929d5375bf0bef532a6b98393afbabd9a310
wajeht <zombyard@gmail.com>
2021-11-26 16:45:27 -0600

    feat: add is_admin field to user table

---
+++ db/migrations/20211126222748_user_adding_isadmin_field.js
@@ -0,0 +1,9 @@
+exports.up = (knex) => {
+  return knex.schema.table("user", (table) => {
+    table.boolean("is_admin").defaultTo(false);
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable("user");
+};
