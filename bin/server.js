7abe14a78bcb598c95f5b93bf1911d084c468f88
wajeht <zombyard@gmail.com>
2021-11-26 21:54:54 -0600

    feat: add script to setup admin accoutn when server started!

--- bin/server.js
+++ bin/server.js
@@ -4,9 +4,11 @@ const chalk = require("chalk");
 const log = console.log;
 const error = chalk.black.bgRed;
 const success = chalk.black.bgGreen;
+const warn = chalk.black.bgYellow;
 
 const { port, database, admin } = require("../config/config.js");
 const db = require("../db/db.js");
+const User = require("../src/models/user.model.js");
 
 // check to see if postgres has started
 const testDatabaseConnection = () => {
@@ -19,6 +21,10 @@ const testDatabaseConnection = () => {
       .catch((err) => {
         reject(err);
         log(error("Cannot start to PostgresSQL database!"));
+      })
+      .finally(() => {
+        log(success(`Done!`));
+        log();
       });
   });
 };
@@ -28,14 +34,48 @@ const checkForDatabase = (databaseName) => {
   return new Promise((resolve, reject) => {
     db.raw("select 1 from pg_database where datname = ? ", [1])
       .then((res) => {
-        log(success(`Check to see if ${databaseName} database exist...`));
+        log(success(`Checking to see if ${databaseName} database exist...`));
         resolve(res);
       })
       .catch((err) => {
         reject(err);
         log(error(`Cannot find ${databaseName} database!`));
+      })
+      .finally(() => {
+        log(success(`Done!`));
+        log();
       });
   });
+};
+
+// check to see if admin exit or has be setup
+const setUpAdminAccount = async (email) => {
+  const adminExist = await User.adminExist(email);
+  log(success(`Checking to see if admin account has been setup...`));
+
+  if (!adminExist.length) {
+    let message = "You have not set up an admin account yet!\n";
+    message += "If you have not sign up, go ahead and make one!\n";
+    message += "Then go .env file and replace admin with your email!";
+    log(error(message));
+  }
+
+  log(success(`Done!`));
+  log();
+
+  log(success(`Setting up admin account for ${email}...`));
+  const setAdmin = await User.setAdmin(email, true);
+
+  if (!setAdmin) {
+    log(
+      error(
+        `Cannot find an account with ${email},\ncheck to see if .evn and your signed up email are correct!`
+      )
+    );
+  }
+
+  log(success(`Done!`));
+  log();
 };
 
 // starting server
@@ -43,6 +83,7 @@ const checkForDatabase = (databaseName) => {
   try {
     const isConnection = await testDatabaseConnection();
     const databaseExist = await checkForDatabase(database.database);
+    const setupAdmin = await setUpAdminAccount(admin);
 
     const app = require("../src/app.js");
     app.listen(port);
@@ -53,4 +94,3 @@ const checkForDatabase = (databaseName) => {
     process.exit(1);
   }
 })();
-
