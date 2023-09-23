718a4b74e1373baac7c932564f549587f138de3d
wajeht <58354193+wajeht@users.noreply.github.com>
2023-09-23 01:51:45 -0500

    feat: add more config

--- config/config.js
+++ config/config.js
@@ -7,6 +7,11 @@ require("dotenv").config({
 
 module.exports = {
   database: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     url: process.env.DATABASE_URL,
   },
   port: process.env.PORT,
